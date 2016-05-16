4d5ef9627aaa937cca528e54e4bdb292f8b18610
johbac <john.backstrom@nordnet.se>
2016-05-16 21:54:14 +0200

    #1 Added an small example of loading dynamic languages

--- src/app.jsx
+++ src/app.jsx
@@ -1,21 +1,54 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import { IntlProvider, addLocaleData } from 'react-intl';
 import TranslatedComponent from './components/translated-component.jsx';
 import sv from 'react-intl/locale-data/sv';
+import en from 'react-intl/locale-data/en';
 import svMessages from '../l10n/sv-SE.json';
+import enMessages from '../l10n/en.json';
 
 addLocaleData(sv);
+addLocaleData(en);
 
-const App = (props) => {
-  return (
-      <IntlProvider locale={ `sv-SE` } messages={ svMessages }>
-        <TranslatedComponent {...props} />
-      </IntlProvider>
-  );
-};
+class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.handleLangChange = this.handleLangChange.bind(this);
+    this.state = {
+      lang: 'sv-SE',
+      messages: svMessages,
+    };
+  }
+
+  handleLangChange(event){
+    const lang = event.target.value;
+    switch(lang){
+      case 'sv-SE':
+        return this.setState({ lang, messages: svMessages});
+      case 'en':
+        return fetch('l10n/en.json')
+          .then(response => response.json())
+          .then(messages => this.setState({lang, messages}));
+    }
+
+  }
+
+  render() {
+    const { lang, messages } = this.state;
+    return (
+      <div>
+        <select value={lang} onChange={this.handleLangChange}>
+          <option value="sv-SE">Swedish</option>
+          <option value="en">English</option>
+        </select>
+        <IntlProvider locale={ lang } messages={ messages }>
+          <TranslatedComponent {...this.props} />
+        </IntlProvider>
+      </div>
+    );
+  }
+}
 
 
 ReactDOM.render(<App/>, document.getElementById('app'));
-
-
--- webpack.config.js
+++ webpack.config.js
@@ -22,4 +22,11 @@ module.exports = {
       },
     ],
   },
+  devServer: {
+    proxy:{
+      'l10n/*': {
+        target: 'http://localhost:8081'
+      }
+    }
+  }
 };
